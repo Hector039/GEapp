19ccb6f9842e0268d7c921d69be8f3762e0c5ba5
Hector <elector22@gmail.com>
2025-05-10 13:50:48 -0300

    other changes to urlDB

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
-export default async function mongoLoader(dburl){
-    try{
-        console.log(dburl);
-        
+export default async function mongoLoader(dburl) {
+    try {
         await mongoose.connect(dburl);
-    }catch(error){
-        throw new Error("Error connecting to Mongo DB", error)
+    } catch (error) {
+        console.log("Error connecting to Mongo DB", error)
     }
 }
